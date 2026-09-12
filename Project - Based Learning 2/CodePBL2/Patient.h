2b0853945e9d4fbeaf1d92dcba87db76eb403b50
Long <nobeasa123@gmail.com>
2026-09-12 08:57:37 +0700

    patient

--- Project - Based Learning 2/CodePBL2/Patient.h
+++ Project - Based Learning 2/CodePBL2/Patient.h
@@ -1,4 +1,17 @@
 #include <iostream>
 #include <string>
 #include "Person.h"
-Class Patient
+class Patient : public Person{
+    private:
+    std::string medicalRecordId;
+    double insuranceDiscount;
+    public:
+    void inputData() override{
+        Person::inputData();
+    }
+    void displayData() const override{
+        Person::displayData();
+    }
+    virtual void viewMedicalHistory(){
+    }
+};
