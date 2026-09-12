5c949c1d3aa72f57d2a48319362ed055a901335b
pretonwanttorevenge <thinhphatkaio810@gmail.com>
2026-09-12 08:41:58 +0700

    12-9-2026

--- Project - Based Learning 2/CodePBL2-Phát/Invoice.cpp
+++ Project - Based Learning 2/CodePBL2-Phát/Invoice.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
 class Invoice {
 private:
     std::string invoiceId;
--- Project - Based Learning 2/CodePBL2-Phát/Medicine.cpp
+++ Project - Based Learning 2/CodePBL2-Phát/Medicine.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
 class Medicine {
 private:
     std::string medicineId;
